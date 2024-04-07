140b1274f8f62b4dbfdb93f6b25c987df29f2f96
NinaZurash <nino.zurashvili.nz@gmail.com>
2024-04-07 23:15:17 +0400

    update sign-in

--- src/components/form/pictureGallery.tsx
+++ src/components/form/pictureGallery.tsx
@@ -1,8 +1,7 @@
 "use client";
 
-import { set } from "zod";
+import { useEffect, useRef, useState } from "react";
 import GalleryImage from "./GalleryImage";
-import { useEffect, useState } from "react";
 
 let imagesArr = [
   [
@@ -13,7 +12,7 @@ let imagesArr = [
   ],
   [
     "https://auth.tnet.ge/templates/assets/img/slider/vendoo.png",
-    "https://auth.tnet.ge/templates/assets/img/slider/myhome.png",
+    "https://auth.tnet.ge/templates/assets/img/slider/swoop-revers.png",
     "https://auth.tnet.ge/templates/assets/img/slider/livo.png",
     "https://auth.tnet.ge/templates/assets/img/slider/vendoo-revers.png",
   ],
@@ -23,13 +22,23 @@ const MAX_STEP = -310;
 
 export default function PictureGallery() {
   const [step, setStep] = useState(MAX_STEP);
+  const intervalRef = useRef<NodeJS.Timeout | undefined>();
 
   useEffect(() => {
-    const interval = setInterval(() => {
+    intervalRef.current = setInterval(() => {
       setStep((prev: number) => prev + 1);
     }, 20);
-    return () => clearInterval(interval);
+    return () => clearInterval(intervalRef.current);
   }, []);
+
+  const handleMouseEnter = () => {
+    if (intervalRef.current) clearInterval(intervalRef.current);
+  };
+  const handleMouseLeave = () => {
+    intervalRef.current = setInterval(() => {
+      setStep((prev: number) => prev + 1);
+    }, 20);
+  };
 
   if (step === 0) {
     imagesArr[0].unshift(imagesArr[0].pop() as string);
@@ -37,20 +46,27 @@ export default function PictureGallery() {
 
     setStep(MAX_STEP);
   }
+
   return (
-    <div className="flex justify-between flex-row gap-10 ml-16">
-      <div className=" overflow-hidden relative">
-        <div className="flex flex-col gap-5  transition-transform">
-          {imagesArr[0].map((src, index) => (
-            <GalleryImage key={index} src={src} position={step} />
-          ))}
+    <div
+      className="flex flex-row ml-16"
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+    >
+      <div className="fixed flex justify-between gap-10 overflow-hidden">
+        <div className=" overflow-hidden relative">
+          <div className="flex flex-col gap-5  transition-transform">
+            {imagesArr[0].map((src, index) => (
+              <GalleryImage key={index} src={src} position={step} />
+            ))}
+          </div>
         </div>
-      </div>
-      <div className="overflow-hidden relative">
-        <div className="flex flex-col gap-5  transition-transform">
-          {imagesArr[1].map((src, index) => (
-            <GalleryImage key={index} src={src} position={MAX_STEP - step} />
-          ))}
+        <div className="overflow-hidden relative">
+          <div className="flex flex-col gap-5  transition-transform">
+            {imagesArr[1].map((src, index) => (
+              <GalleryImage key={index} src={src} position={MAX_STEP - step} />
+            ))}
+          </div>
         </div>
       </div>
     </div>
